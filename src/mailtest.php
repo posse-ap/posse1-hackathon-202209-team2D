f5141f401a0dc06737c87e69aeffcc68c94d499e
kotani <silent.bayline@gmail.com>
2021-08-20 04:12:46 +0900

    サンプル実装

--- src/mailtest.php
+++ src/mailtest.php
@@ -6,13 +6,13 @@ mb_internal_encoding('UTF-8');
 $to = "hackathon-teamX@posse-ap.com";
 $subject = "PHPからメール送信サンプル";
 $body = "本文";
-$headers = ['From'=>'system@posse-ap.com', 'Content-Type'=>'text/plain; charset=UTF-8', 'Content-Transfer-Encoding'=>'8bit'];
+$headers = ["From"=>"system@posse-ap.com", "Content-Type"=>"text/plain; charset=UTF-8", "Content-Transfer-Encoding"=>"8bit"];
 
 $name = "テスト";
 $date = "2021年08月01日（日） 21:00~23:00";
 $event = "縦モク";
 $body = <<<EOT
-名前:{$name}さん
+{$name}さん
 
 ${date}に${event}を開催します。
 参加／不参加の回答をお願いします。
@@ -21,3 +21,4 @@ http://localhost/
 EOT;
 
 mb_send_mail($to, $subject, $body, $headers);
+echo "メールを送信しました";
